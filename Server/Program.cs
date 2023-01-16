f403a714752ee2b58bfb99c8c30abc9960c39726
lbynbplus <lvboyu2001@gmail.com>
2023-01-16 16:34:08 +1300

    First basic submission

---
+++ Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+
+namespace Server
+{
+  class Program
+    {
+       static void Main(string[] args)
+        {
+            WebSocketServer Server = new WebSocketServer("ws://127.0.0.1:8205");
+            Server.Start();
+            Console.WriteLine("Server Start");
+
+            Console.ReadKey();
+            Server.Stop();
+
+        }
+    }
+}
