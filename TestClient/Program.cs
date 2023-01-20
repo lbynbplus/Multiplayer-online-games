471c0457ca1114dcce6b4cfc6a3a2e8c84acaaf5
lbynbplus <lvboyu2001@gmail.com>
2023-01-20 14:15:09 +1300

    protobuf

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -7,11 +7,13 @@ namespace TestClient
     {
         static void Main(String[] args)
         {
-            WebSocket ws = new WebSocket("ws://127.0.0.1:8205/test");
-            ws.Connect();
-            ws.Send("test msg");
-            Console.ReadKey();
-            ws.Close();
+            using (WebSocket ws = new WebSocket("ws://127.0.0.1:8205/test"))
+            {
+                ws.Connect();
+                ws.Send("test msg");
+                Console.ReadKey();
+                ws.Close();
+            }
         }
     }
 }
