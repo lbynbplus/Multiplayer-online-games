a1cded8456b0566e9f1cbf0420f7a14f2e9ad557
Rusira Jayatilake <119916752+RusiraJayatilake@users.noreply.github.com>
2023-01-27 22:07:46 +1300

    Add files via upload

---
+++ Server/Data/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Data
+{
+    public class Player
+    {
+        public int position { get; set; }
+        public string playername { get; set; }
+        public int playerid { get; set; } = 1;
+    }
+}
