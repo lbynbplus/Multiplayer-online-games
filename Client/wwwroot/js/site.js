954a3d3f202518be1fcf133b5e9700fe0be47c4a
Rusira Jayatilake <rusirajayatilake96@gmail.com>
2023-02-08 02:04:04 +1300

    Game Room Design

--- Client/wwwroot/js/site.js
+++ Client/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function diesRoll() {
+    var imageArr = new Array();
+
+    imageArr[0] = "images/dice/dice1.png";
+    imageArr[1] = "images/dice/dice2.png";
+    imageArr[2] = "images/dice/dice3.png";
+    imageArr[3] = "images/dice/dice4.png";
+    imageArr[4] = "images/dice/dice5.png";
+    imageArr[5] = "images/dice/dice6.png";
+
+    var num = Math.floor(Math.random() * imageArr.length);
+
+    return document.getElementById('dice-img').innerHTML = '<img src="' + imageArr[num] + '"/>';
+}
